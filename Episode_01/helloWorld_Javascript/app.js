fcf138e835d8218894c2c4ef9e7edfb2b56b486e
shivsingh98 <singhshiv0402@gmail.com>
2023-10-30 20:22:26 +0530

    Episode_01

---
+++ Episode_01/helloWorld_Javascript/app.js
@@ -0,0 +1,7 @@
+const heading = document.createElement("h1");
+
+heading.innerHTML = "Hello World from Javascript!"
+
+const root = document.getElementById("root");
+
+root.appendChild(heading);
---
+++ Episode_02/App.js
@@ -0,0 +1,13 @@
+const main = React.createElement("div", {id: "main"},
+[
+ React.createElement("h1", {id: "main-heading"}, "First React Project"),
+ [
+ React.createElement("div", {id: "box1"}, 
+ React.createElement("div", {id: "box2"},
+ React.createElement("div", {id: "box3"},
+ React.createElement("h3", {id: "name", style:{color:"blue"}}, "Zeeva Singh")
+ )))]
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(main);
+
