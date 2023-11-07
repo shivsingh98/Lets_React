7ff9a3751be358f46a3bf40e42f79f6b2ba717c3
shivsingh98 <singhshiv0402@gmail.com>
2023-11-07 11:40:00 +0530

    Episode 02 completed

--- Episode_02/App.js
+++ Episode_02/App.js
@@ -1,13 +1,29 @@
-const main = React.createElement("div", {id: "main"},
-[
- React.createElement("h1", {id: "main-heading"}, "First React Project"),
- [
- React.createElement("div", {id: "box1"}, 
- React.createElement("div", {id: "box2"},
- React.createElement("div", {id: "box3"},
- React.createElement("h3", {id: "name", style:{color:"blue"}}, "Zeeva Singh")
- )))]
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(main);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+const main = React.createElement('div', { id: 'main' }, [
+  React.createElement('h1', { id: 'main-heading' }, 'First React Project'),
+  [
+    React.createElement(
+      'div',
+      { id: 'box1' },
+      React.createElement(
+        'div',
+        { id: 'box2' },
+        React.createElement(
+          'div',
+          { id: 'box3' },
+          React.createElement(
+            'h3',
+            { id: 'name', style: { color: 'blue' } },
+            'Zeeva Singh'
+          )
+        )
+      )
+    ),
+  ],
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(main);
